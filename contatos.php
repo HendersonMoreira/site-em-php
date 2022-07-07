b595992d2026910fef16eb613b571c4ffbca7727
HendersonMoreira <103003690+HendersonMoreira@users.noreply.github.com>
2022-07-06 21:38:38 -0300

    Add files via upload

---
+++ contatos.php
@@ -0,0 +1,17 @@
+<?php include('_header.php'); ?>
+
+<article>
+
+    <h2>Faça Contato</h2>
+    <p>Preencha todos os campos para entrar em contato.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis obcaecati deserunt, culpa sunt odio alias ipsam voluptas, optio commodi accusantium ratione? At nulla porro accusamus perferendis tenetur? Aliquid, fugiat eos!</p>
+
+</article>
+
+<aside>
+
+    <h3>Redes sociais</h3>
+
+</aside>
+
+<?php include('_footer.php'); ?>
