a8dfd8ee145e130ec4be97907b926c098d64895e
HendersonMoreira <103003690+HendersonMoreira@users.noreply.github.com>
2022-07-11 21:48:07 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
 
 /**
- * Título desta página:
+ * Define i título desta página:
  * 
  * A variável '$page_title' especifica o título da página, que aparece 
  * na tag <title>...</title>. Esta variável não pode ser vazia.
  * 
  * Conforme o case, a tag title terá os seguintes formatos:
+ * 
  *  • Na página inicial → <title>Nome do site .:. Slogan do site</title>
  *  • Em páginas estáticas → <title>Nome do site .:. Nome da página</title>
  *  • Na página de artigos → <title>Nome do site .:. Título do artigo</title>
@@ -20,14 +21,14 @@ require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
 $page_title = 'Página modelo';
 
 /**
- * Conteúdo desta página:
+ * Define o conteúdo principal desta página:
  * 
  * Esta viarável será exibida dentro da tag <article>...</article>.
  */
 $page_article = '';
 
 /**
- * Barra lateral desta página:
+ * Define o conteúdo da barra lateral desta página:
  * 
  * Esta variável será exibida na tag <aside>...</aside>.
  */
@@ -37,14 +38,52 @@ $page_aside = '';
  * Todo o código PHP desta página começa aqui! *
  ***********************************************/
 
+// Obtém todos os artigos do site:
+$sql = <<<SQL
 
+SELECT 
+	art_id, art_title, art_thumb, art_intro
+FROM `articles`
+WHERE art_status = 'on'
+	AND art_date <= NOW()
+ORDER BY art_date DESC;
+
+SQL;
+
+// Executa a query:
+$res = $conn->query($sql);
+
+$page_article = <<<HTML
+
+<h2>Artigos Recentes</h2>
+
+HTML;
+
+// Loop que lista cada registro recebido:
+while( $art = $res->fetch_assoc() ):
+
+    $page_article .= <<<HTML
+
+<div class="art_block" onclick="location.href = '/view/?{$art['art_id']}'">
+
+    <img src="{$art['art_thumb']}" alt="{$art['art_title']}">
+    <div>
+        <h4>{$art['art_title']}</h4>
+        <span>{$art['art_intro']}</span>
+    </div>
+
+</div>
+
+HTML;  
+
+endwhile;
 
 /***********************************
  * Fim do código PHP desta página! *
  ***********************************/
 
 /**
- * Inclui o cabeçalhodo template nesta página:
+ * Inclui o cabeçalho do template nesta página:
  */
 require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
 
